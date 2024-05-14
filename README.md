8780a0be6efa642d6f5d2e5dbbe63e5bd579c41c
mohamedhani <mohamedhani01014@gmail.com>
2024-05-14 01:56:30 +0000

    add pr-comment code

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 ![CI](https://github.com/actions/javascript-action/actions/workflows/ci.yml/badge.svg)
 
 It is a demo github action to create comment of you Pull Request with:
+
 - Number of added, modified, and deleted files
 - Labels on PR with the types of files in this pull request
-[PR-Comment](https://github.com/mohamedhani/pr-comment).
+  [PR-Comment](https://github.com/mohamedhani/pr-comment).
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core')
-const { wait } = require('./wait')
+const github = require('@actions/github')
 
 /**
  * The main function for the action.
@@ -7,18 +7,74 @@ const { wait } = require('./wait')
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    const owner = core.getInput('owner', { required: true })
+    const repo = core.getInput('repo', { required: true })
+    const prNumber = core.getInput('pr_number', { required: true })
+    const token = core.getInput('token', { required: true })
+    const octokit = github.getOctokit(token)
+    const { data: files } = await octokit.rest.pulls.listFiles({
+      owner,
+      repo,
+      pull_number: prNumber
+    })
+    let filesStatus = {
+      addedFiles: 0,
+      modifiedFiles: 0,
+      deletedFile: 0
+    }
+    filesStatus = files.reduce((acm, file) => {
+      acm.addedFiles += file.status === 'added' ? 1 : 0
+      acm.modifiedFiles += file.status === 'modified' ? 1 : 0
+      acm.deletedFile += file.status === 'removed' ? 1 : 0
+    }, filesStatus)
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: prNumber,
+      body: `The Status Of files for Pull Request ${prNumber} is 
+              Added Files: ${filesStatus.addedFiles}
+              Modified Files: ${filesStatus.modifiedFiles}
+              Deleted Files: ${filesStatus.deletedFile}
+      `
+    })
+    const labels = new Set()
+    for (const file in files) {
+      const extention = file.filename.split('.').pop()
+      switch (extention) {
+        case 'md':
+          labels.add('docs')
+          break
+        case 'yml':
+        case 'yaml':
+          labels.add('yaml')
+          break
+        case 'go':
+          labels.add('golang')
+          break
+        case 'js':
+        case 'ts':
+          labels.add('javascript')
+          break
+        case 'py':
+          labels.add('python')
+          break
+        case 'java':
+          labels.add('java')
+          break
+        case 'gem':
+          labels.add('ruby')
+          break
+        default:
+          labels.add('undefined')
+      }
+    }
+    octokit.rest.issues.addLabels({
+      owner,
+      repo,
+      issue_number: prNumber,
+      labels: Array.from(labels)
+    })
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
